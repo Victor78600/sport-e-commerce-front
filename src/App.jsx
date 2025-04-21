4de947a28ccf6e76dcaa3f89f145f02381fb09b0
victor duveau <victor.duveau@abtasty.com>
2025-04-21 12:24:39 +0200

    homepage créer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+// import ProfilPage from "./pages/ProfilPage";
+import HomePage from "./pages/homepage";
+// import CreateActivity from "./pages/CreateActivityPage";
+// import OneActivityPage from "./pages/OneActivityPage";
+// import EditProfile from "./pages/EditProfile";
+// import CreateComment from "./pages/CreateComment";
+// import UpdateActivity from "./pages/UpdateActivity";
+// Navigation
+// import LoggedOutUser from "./navigation/LoggedOutUser";
+// import LoggedInUser from "./navigation/LoggedInUser";
+
+// import Layout from "./layouts/Layout";
+// import UserLayout from "./layouts/UserLayout";
+// import SignUpPage from "./pages/SignUpPage";
+// import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Routes>
+            <Route index element={<HomePage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,19 @@
+// import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+// import UserDisplayer from "./../components/UserDisplayer/UserDisplayer";
+function HomePage() {
+//   const { isLoggedIn } = useAuth();
+
+//   if (!isLoggedIn) {
+    return (
+      <h1>Site e-commerce (magasin de sport)</h1>
+      // <p className="HomeLogin">
+        
+      //   Please <Link to="/login">Log in</Link>
+      // </p>
+    );
+  }
+//   return <UserDisplayer />;
+// }
+
+export default HomePage;
